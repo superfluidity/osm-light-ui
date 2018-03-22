82cdcf86a5441a19319692db61fa0d4eae51b6f1
Francesco Lombardo <franclombardo@gmail.com>
2018-03-22 11:52:11 +0100

    alignment fix

--- code/lib/osm/osmclient/client.py
+++ code/lib/osm/osmclient/client.py
@@ -171,19 +171,8 @@ class Client(object):
         return Util.json_loads_byteified(r.text)
 
     def md5(self, f):
-        #hash_md5 = hashlib.md5()
-        #with open(fname, "rb") as f:
-        #    for chunk in iter(lambda: f.read(4096), b""):
-        #        hash_md5.update(chunk)
-        #return hash_md5.hexdigest()
         hash_md5 = hashlib.md5()
         for chunk in iter(lambda: f.read(1024), b""):
             hash_md5.update(chunk)
         return hash_md5.hexdigest()
 
-    def f(self, fname):
-        hash_md5 = hashlib.md5()
-        with open(fname, "rb") as f:
-           for chunk in iter(lambda: f.read(1024), b""):
-               hash_md5.update(chunk)
-        return hash_md5.hexdigest()
