f43d23347c1eb8723deae89845637d0a13dfd1e9
Francesco Lombardo <franclombardo@gmail.com>
2018-03-22 18:41:56 +0100

    osm api v2 work in progress

--- code/lib/osm/osmclient/client.py
+++ code/lib/osm/osmclient/client.py
@@ -10,30 +10,26 @@ log = logging.getLogger('helper.py')
 
 
 class Client(object):
-    def __init__(self, host=None, so_port=9999, so_project='admin', ro_host=None, ro_port=9090, **kwargs):
+    def __init__(self, host="192.168.100.212", so_port=9999, so_project='admin', ro_host=None, ro_port=9090, **kwargs):
 
         self._user = 'admin'
         self._password = 'admin'
         # self._project = so_project
         self._project = so_project
-        self._auth_endpoint = '/token/v1'
-        self._headers = {}
-        # if len(host.split(':')) > 1:
-        #     # backwards compatible, port provided as part of host
-        #     self._host = host.split(':')[0]
-        #     self._so_port = host.split(':')[1]
-        # else:
-        #     self._host = host
-        #     self._so_port = so_port
+        self._token_endpoint = 'admin/v1/tokens'
+        self._user_endpoint = 'admin/v1/users'
 
-        if ro_host is None:
-            ro_host = host
+        self._headers = {}
+        self._host = host
+
+        self._base_path = "https://{0}:{1}/osm".format(self._host, so_port)
 
     def get_token(self):
         postfields_dict = {'username': self._user,
                            'password': self._password,
                            'project-id': self._project}
-        token = self._send_post('https://192.168.100.212:9999/osm/token/v1', None, postfields_dict, headers={"Content-Type": "application/yaml", "accept": "application/json"})
+        token_url = "{0}/{1}".format(self._base_path, self._token_endpoint)
+        token = self._send_post(token_url, None, postfields_dict, headers={"Content-Type": "application/yaml", "accept": "application/json"})
         if token is not None:
             return token['id']
         return None
@@ -44,7 +40,8 @@ class Client(object):
             self._headers['Authorization'] = 'Bearer {}'.format(token)
             self._headers['Content-Type'] = 'application/yaml'
             self._headers['accept'] = 'application/json'
-            return self._send_get("https://192.168.100.212:9999/osm/nsd/v1/ns_descriptors", headers=self._headers)
+            _url = "{0}/nsd/v1/ns_descriptors_content".format(self._base_path)
+            return self._send_get(_url, headers=self._headers)
         return None
 
     def nsd_get(self, id):
@@ -53,7 +50,8 @@ class Client(object):
             self._headers['Authorization'] = 'Bearer {}'.format(token)
             self._headers['Content-Type'] = 'application/yaml'
             self._headers['accept'] = 'application/json'
-            return self._send_get("https://192.168.100.212:9999/osm/nsd/v1/ns_descriptors/{}".format(id), headers=self._headers)
+            _url = "{0}/nsd/v1/ns_descriptors_content/{1}".format(self._base_path,id)
+            return self._send_get(_url, headers=self._headers)
         return None
 
     def nsd_delete(self, id):
@@ -62,7 +60,8 @@ class Client(object):
             self._headers['Authorization'] = 'Bearer {}'.format(token)
             self._headers['Content-Type'] = 'application/yaml'
             self._headers['accept'] = 'application/json'
-            return self._send_delete("https://192.168.100.212:9999/osm/nsd/v1/ns_descriptors/{}".format(id), headers=self._headers)
+            _url = "{0}/nsd/v1/ns_descriptors_content/{1}".format(self._base_path, id)
+            return self._send_delete(_url, headers=self._headers)
         return None
 
     def nsd_onboard(self, package):
@@ -72,21 +71,15 @@ class Client(object):
 
             headers['Authorization'] = 'Bearer {}'.format(token)
             headers['Content-Type'] = 'application/gzip'
-            #headers['Content-File-MD5'] = self.md5file('cirros_2vnf_ns.tar.gz')
-            #print self.md5(package.file)
-            #print self.md5file('cirros_2vnf_ns.tar.gz')
-            #files = {'file': ('cirros_2vnf_ns.tar.gz', open('cirros_2vnf_ns.tar.gz', 'rb'), 'application/gzip')}
-            #print headers['Content-File-MD5']
             headers['accept'] = 'application/json'
-            #print package.read()
-            #print (package.name)
             with open('/tmp/'+package.name, 'wb+') as destination:
                 for chunk in package.chunks():
                     destination.write(chunk)
             headers['Content-File-MD5'] = self.md5(open('/tmp/'+package.name, 'rb'))
             #print type(open('cirros_2vnf_ns.tar.gz', 'rb').read())
             #r = requests.post(url='http://upload.example.com', data={'title': 'test_file},  files =  {'file':package})
-            return self._send_post("https://192.168.100.212:9999/osm/nsd/v1/ns_descriptors", headers=headers,
+            _url = "{0}/nsd/v1/ns_descriptors_content/".format(self._base_path)
+            return self._send_post(_url, headers=headers,
                                   data=open('/tmp/'+package.name, 'rb'))
         return None
 
@@ -96,7 +89,8 @@ class Client(object):
             self._headers['Authorization'] = 'Bearer {}'.format(token)
             self._headers['Content-Type'] = 'application/yaml'
             self._headers['accept'] = 'application/json'
-            return self._send_get("https://192.168.100.212:9999/osm/vnfpkgm/v1/vnf_packages", headers=self._headers)
+            _url = "{0}/vnfpkgm/v1/vnf_packages_content".format(self._base_path)
+            return self._send_get(_url, headers=self._headers)
         return None
 
     def vnfd_get(self, id):
@@ -105,7 +99,8 @@ class Client(object):
             self._headers['Authorization'] = 'Bearer {}'.format(token)
             self._headers['Content-Type'] = 'application/yaml'
             self._headers['accept'] = 'application/json'
-            return self._send_get("https://192.168.100.212:9999/osm/vnfpkgm/v1/vnf_packages/{}".format(id), headers=self._headers)
+            _url = "{0}/vnfpkgm/v1/vnf_packages_content/{1}".format(self._base_path, id)
+            return self._send_get(_url, headers=self._headers)
         return None
 
     def vnfd_delete(self, id):
@@ -114,7 +109,8 @@ class Client(object):
             self._headers['Authorization'] = 'Bearer {}'.format(token)
             self._headers['Content-Type'] = 'application/yaml'
             self._headers['accept'] = 'application/json'
-            return self._send_delete("https://192.168.100.212:9999/osm/vnfpkgm/v1/vnf_packages/{}".format(id), headers=self._headers)
+            _url = "{0}/vnfpkgm/v1/vnf_packages_content/{1}".format(self._base_path, id)
+            return self._send_delete(_url, headers=self._headers)
         return None
 
     def vnfd_onboard(self, package):
@@ -128,7 +124,8 @@ class Client(object):
                 for chunk in package.chunks():
                     destination.write(chunk)
             headers['Content-File-MD5'] = self.md5(open('/tmp/'+package.name, 'rb'))
-            return self._send_post("https://192.168.100.212:9999/osm/vnfpkgm/v1/vnf_packages", headers=headers,
+            _url = "{0}/vnfpkgm/v1/vnf_packages_content".format(self._base_path)
+            return self._send_post(_url, headers=headers,
                                    data=open('/tmp/' + package.name, 'rb'))
         return None
 
